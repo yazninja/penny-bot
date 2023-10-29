6f8e5fede85481c84c95cf4fa48311f0b34c7cf7
yazninja <yazninja@cider.sh>
2023-10-29 06:45:12 +0300

    fix reassure

--- src/commands/wg/reassure.ts
+++ src/commands/wg/reassure.ts
@@ -5,8 +5,8 @@ export const command = {
     data: new SlashCommandBuilder().setName('reassure').setDescription('Penny will reassure you'),
     category: "Writer's",
     async execute(interaction: ChatInputCommandInteraction) {
-        if (interaction.channelId != WGChannels.prompts) {
-            let reply = await interaction.reply({ content: `This command can only be used in <#${WGChannels.prompts}>.` });
+        if (interaction.channelId != WGChannels.reassurance) {
+            let reply = await interaction.reply({ content: `This command can only be used in <#${WGChannels.reassurance}>.` });
             return setTimeout(async () => {
                 await reply.delete();
             }, 10000);
